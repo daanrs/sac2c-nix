395bbbf8eaa8e3f7a581981a2bd9c3e00868a234
daanrs <git@daan.rs>
2026-02-14 14:29:00 +0100

    overlay.nix: use other overlay style

--- overlay.nix
+++ overlay.nix
@@ -1,13 +1,17 @@
-final: prev: {
-  sac2c = final.callPackage ./sac2c { };
-
-  sac2c-stdlib = final.callPackage ./sac2c-stdlib { };
-
-  sac2c-with-stdlib = final.wrap-sac2c final.sac2c final.sac2c-stdlib;
-
+final: prev:
+let
   wrap-sac2c =
     prevSac2c: package:
-    final.writeShellScriptBin "sac2c" ''
+    prev.writeShellScriptBin "sac2c" ''
       exec ${prevSac2c}/bin/sac2c -L "${package}/lib" -T "${package}/lib" "$@"
     '';
+in
+{
+  inherit wrap-sac2c;
+
+  sac2c = prev.callPackage ./sac2c { };
+
+  sac2c-stdlib = prev.callPackage ./sac2c-stdlib { };
+
+  sac2c-with-stdlib = wrap-sac2c final.sac2c final.sac2c-stdlib;
 }
