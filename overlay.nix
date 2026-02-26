4a9c92a1365fa3c1400901d52cca534e29f06c56
daanrs <git@daan.rs>
2026-02-26 11:13:07 +0100

    overlay.nix: better sac2c-with-stlib package

--- overlay.nix
+++ overlay.nix
@@ -1,13 +1,7 @@
 final: prev: {
-  sac2c = final.callPackage ./sac2c { };
+  sac2c = prev.callPackage ./sac2c { };
 
-  sac2c-stdlib = final.callPackage ./sac2c-stdlib { };
+  sac2c-stdlib = prev.callPackage ./sac2c-stdlib { };
 
-  sac2c-with-stdlib = final.wrap-sac2c final.sac2c final.sac2c-stdlib;
-
-  wrap-sac2c =
-    prevSac2c: package:
-    final.writeShellScriptBin "sac2c" ''
-      exec ${prevSac2c}/bin/sac2c -L "${package}/lib" -T "${package}/lib" "$@"
-    '';
+  sac2c-with-stdlib = prev.callPackage ./sac2c-with-stdlib { };
 }
